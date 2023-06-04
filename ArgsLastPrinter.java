6c28bbed731a4b2548bfe677e3edd19b9748a47a
ryota <otama1256@gmail.com>
2023-06-04 21:23:03 +0900

    add new file

--- ArgsLastPrinter.java
+++ ArgsLastPrinter.java
@@ -1,7 +1,7 @@
 public class ArgsLastPrinter{
  public static void main (String[] args) {
   if (args.length == 0) {
-    System.out.println("‚È‚µ");
+    System.out.println("‚ ‚è");
   } else {
     System.out.println(7);
   }
