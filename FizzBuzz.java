998edce84be08b003084b60ffbb40a4cee8fe747
ryota <otama1256@gmail.com>
2023-07-06 19:52:50 +0900

    インデント調整、変数名の変更

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,27 +1,19 @@
 public class FizzBuzz {
-   public static void main (String[] args) {
-
-     int a = Integer.parseInt(args[0]);
-
-     for (int i = 1; i <= a; i++) {
-
-        if (args.length == 0) {
-			return;
-        }    
-
-        if ((i % 3 ==0) && (i % 5 == 0)) {
-            System.out.print("FizzBuzz" + " ");
-
-        } else if (i % 3 == 0) {
-            System.out.print("Fizz" + " ");
-
-        } else if (i % 5 == 0) {
-            System.out.print("Buzz" + " ");
-
-        } else {
-            System.out.print(i + " ");
-            
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        for (int i = 1; i <= num; i++) {
+            if (args.length == 0) {
+                return;
+            }
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.print("FizzBuzz" + " ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz" + " ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz" + " ");
+            } else {
+                System.out.print(i + " ");
+            }
         }
-     }
-   }  
+    }
 }
