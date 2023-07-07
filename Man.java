fd48fa56721f8c6159ea35908e319fe37004fbd5
ryota <otama1256@gmail.com>
2023-07-07 18:46:02 +0900

    11. 文法 - 基本 3 課題２

---
+++ Man.java
@@ -0,0 +1,9 @@
+public class Man extends AbstractPerson {
+
+    private String gender = "man";
+
+    @Override
+    protected String getGender() {
+        return this.gender;
+    }
+}
---
+++ Woman.java
@@ -0,0 +1,8 @@
+public class Woman extends AbstractPerson {
+    
+    private String gender = "woman";
+
+    protected String getGender() {
+		return this.gender;
+    }
+}
